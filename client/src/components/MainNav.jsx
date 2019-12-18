5f2a7fc7567c614ebf29f3985da2369bdc7a21a1
yatheeshraju <yatheesh.konduru@gmail.com>
2019-12-18 22:54:15 +0530

    side bar

--- client/src/components/MainNav.jsx
+++ client/src/components/MainNav.jsx
@@ -1,57 +1,64 @@
-import React, { Component } from 'react';
-import {Menu,Icon} from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Menu, Icon, Sidebar, Segment, Header, Image } from "semantic-ui-react";
+import RightNav from "./RightNav";
 
 export default class MainNav extends Component {
+  state = { visible: false };
 
-    state = {
-
-    }
-
-    handleItemClick= (e)=>function () {
-        this.setState({ activeItem: e.name });
+  handleItemClick = e =>
+    function() {
+      this.setState({ activeItem: e.name });
     };
+  handleSidebarHide = () => this.setState({ visible: false });
+  handleShowClick = () => this.setState({ visible: true });
 
-    
+  render() {
+    const { activeItem } = this.state;
+    const { visible } = this.state;
+    const logostyle = {
+      textAlign: "center",
+      paddingTop: "15px",
+      fontSize: "24px",
+      fontFamily: "Roboto"
+    };
+    return (
+      <div>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation="push"
+            direction="right"
+            onHide={this.handleSidebarHide}
+            vertical
+            visible={visible}
+            width="thin"
+          >
+            <RightNav></RightNav>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Menu size="massive" attached="top">
+              <Menu.Item position="left" name="trackers">
+                <Icon name="eye" />
+              </Menu.Item>
 
-    render() {
-        const { activeItem } = this.state;
-        const logostyle={
-            textAlign:"center",
-            paddingTop:"15px",
-            fontSize:'24px',
-            fontFamily:'Roboto',
-        };
-        return (
-            <div>
-                <Menu size='massive' attached='top'>
-                    <Menu.Item
-                        position='left'
-                        name='trackers'
-                        active={activeItem === 'trackers'}
-                        onClick={this.handleItemClick}
-                    >
-                        <Icon name='eye' />
-                    </Menu.Item>
-                    
-                    <div style={logostyle}> TrackEye</div>
-                    
-                    
-                    <Menu.Item
-                        position='right'
-                        name='options'
-                        active={activeItem === 'options'}
-                        onClick={this.handleItemClick}
-                    >
-                        <Icon name='options' />
-                    </Menu.Item>
-                </Menu>
+              <div style={logostyle}> TrackEye</div>
 
-                
-            </div>
-        )
-
-        
-    }
-
-   
+              <Menu.Item
+                position="right"
+                name="options"
+                active={activeItem === "options"}
+                onClick={this.handleShowClick}
+              >
+                <Icon name="options" />
+              </Menu.Item>
+            </Menu>
+            <Segment basic width={Window.width}>
+              <Header as="h3">Application Content</Header>
+              <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    );
+  }
 }
--- client/src/components/RightNav.jsx
+++ client/src/components/RightNav.jsx
@@ -1,41 +1,19 @@
 import React, { Component } from 'react';
-import {Menu,Sidebar} from 'semantic-ui-react';
+import {Menu} from 'semantic-ui-react';
 
 export default class RightNav extends Component {
 
-    state={
-        visible:'',
-    }
-
+    
     render() {
+
         return (
             <div>
-                <Sidebar
-                    as={Menu}
-                    animation='overlay'
-                    icon='labeled'
-                    inverted
-                    onHide={() => setVisible(false)}
-                    vertical
-                    visible={visible}
-                    width='thin'>
-
-                    <Menu.Item
-                        name='ctracker'
-                        active={activeItem === 'ctracker'}
-                        onClick={this.handleItemClick}
-                    >
-                     Create Tracker    
-                    </Menu.Item>
-                    <Menu.Item
-                        name='etracker'
-                        active={activeItem === 'etracker'}
-                        onClick={this.handleItemClick}
-                    >
-                     Edit Tracker    
-                    </Menu.Item>
-        </Sidebar>
-            </div>
+                <Menu.Item header> Options </Menu.Item>
+                <Menu.Item as='a'>Create Tracker</Menu.Item>
+                <Menu.Item as='a'>Edit Tracker</Menu.Item>
+                
+         </div>
+            
         )
     }
 }
